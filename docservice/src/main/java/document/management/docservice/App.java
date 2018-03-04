41b3129863bd86575b576762380869a60a579cda
admin <admin@10.0.0.142>
2018-03-03 17:53:23 -0800

    first git project

---
+++ docservice/src/main/java/document/management/docservice/App.java
@@ -0,0 +1,13 @@
+package document.management.docservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
